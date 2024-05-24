24bdbe39c5c23c6c2183e0aa2fb37e09e2fb4f23
s1nfyy <blogerslu@gmail.com>
2024-05-24 01:50:26 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+var config = {
+    lines: [
+        "Cuando te conocí, nunca imaginé que llegarías a ser tan especial para mí. Con el tiempo, nuestra amistad se llenó de cariño y risas, y sin darme cuenta, me fui enamorando de ti. Aunque nunca te lo confesé por miedo al rechazo, me sorprendió descubrir que también te habías enamorado de mí, y di el primer paso para conquistarnos.",
+        "Hoy, después de tanto tiempo juntos, puedo decir que cada momento contigo es inolvidable. Me haces sentir muy especial y muy feliz, y agradezco profundamente tu amor y dedicación. Prometo luchar por nuestro amor y construir un futuro juntos. Te amaré siempre y hasta el infinito y más allá. ¡Te amo puta!                ❤️",
+    ],
+}
